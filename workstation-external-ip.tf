b5d50639e696505484507a8c6461f9bfe7a13a3d
Shin DongSu <78459621+Shin9184@users.noreply.github.com>
2021-09-13 16:19:33 +0900

    terraform-eks

--- workstation-external-ip.tf
+++ workstation-external-ip.tf
@@ -7,6 +7,7 @@
 # configure inbound EC2 Security Group access
 # to the Kubernetes cluster.
 #
+# 이 구성은 필요하지 않으며 Kubernetes 클러스터에 대한 인바운드 EC2 Security Group 액세스를 구성하기 위해 로컬 워크스테이션의 외부 IP를 쉽게 가져오기 위한 예제로만 제공됩니다.
 
 data "http" "workstation-external-ip" {
   url = "http://ipv4.icanhazip.com"
